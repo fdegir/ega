0fd605517cccdef9822f2b4ef5a97087fb690eb3
Fatih Degirmenci <fdegir@gmail.com>
2021-09-01 17:25:50 +0200

    Update action

--- entrypoint.sh
+++ entrypoint.sh
@@ -74,11 +74,12 @@ EOF
 
 remrem_generate_service_url=$1
 remrem_publish_service_url=$2
-#REMREM_COMMAND="curl -X GET --header 'Accept: application/json' \"http://89.46.83.162:8081/event_types/$EIFFEL_MP\""
-#REMREM_COMMAND="curl http://89.46.83.162:8081/event_types/$EIFFEL_MP"
-#echo "Info  : Command to run is"
-#echo "        $REMREM_COMMAND"
-#event_list=$($REMREM_COMMAND)
+# set some vars
+GITHUB_REPO_URI="https://github.com/$GITHUB_REPOSITORY"
+EIFFEL_MSG_FILE="$GITHUB_WORKSPACE/eiffel_event.json"
+
+rm -rf "$EIFFEL_MSG_FILE"
+
 echo "Info  : Generating and publishing Eiffel Event"
 echo "-------------------------------------------------"
 echo "        Eiffel Message Protocol:  $eiffel_mp"
@@ -86,12 +87,6 @@ echo "        Eiffel Message Type: $eiffel_message_type"
 echo "        Eiffel REMReM Generate Service URL: $remrem_generate_service_url"
 echo "        Eiffel REMReM Publish Service URL: $remrem_publish_service_url"
 echo "-------------------------------------------------"
-
-# set some vars
-GITHUB_REPO_URI="https://github.com/$GITHUB_REPOSITORY"
-EIFFEL_MSG_FILE="$GITHUB_WORKSPACE/eiffel_event.json"
-
-rm -rf "$EIFFEL_MSG_FILE"
 
 case ${eiffel_message_type} in
   EiffelSourceChangeCreatedEvent)
@@ -106,7 +101,7 @@ case ${eiffel_message_type} in
     ;;
 esac
 
-PUBLISH_URL="${remrem_publish_service_url}?mp=${EIFFEL_MP}&msgType=${EIFFEL_MSG_TYPE}"
+PUBLISH_URL="${remrem_publish_service_url}?mp=${eiffel_mp}&msgType=${eiffel_msg_type}"
 PUBLISH_CMD="curl -H Content-Type:application/json -X POST --data-binary @$EIFFEL_MSG_FILE $PUBLISH_URL"
 
 echo "Info  : Generated event $eiffel_message_type"
