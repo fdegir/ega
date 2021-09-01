22afac8f62f88bc61033ec264c1042cb9d21f4a5
Fatih Degirmenci <fdegir@gmail.com>
2021-09-01 12:13:28 +0200

    Update entrypoint.sh and the version of action to use

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh -l
 
 echo "Info  : Querying Eiffel REMReM Generate Service for list of Eiffel Events for Message Protocol $mp"
 event_list=$(curl -X GET --header "Accept: application/json" "http://89.46.83.162:8081/event_types/$mp")
