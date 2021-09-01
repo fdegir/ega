bb1a7ae58103534d873ec3cd4392d302398d6a48
Fatih Degirmenci <fdegir@gmail.com>
2021-09-01 12:21:38 +0200

    Update action

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh -l
 
 echo "Info  : Querying Eiffel REMReM Generate Service for list of Eiffel Events for Message Protocol $mp"
-event_list=$(curl -X GET --header "Accept: application/json" "http://89.46.83.162:8081/event_types/$mp")
+REMREM_COMMAND="curl -X GET --header \"Accept: application/json\" \"http://89.46.83.162:8081/event_types/$mp\""
+echo "Info  : Command to run is"
+echo "        $REMREM_COMMAND"
+event_list=$($REMREM_COMMAND)
 echo "::set-output name=event_list::$event_list"
