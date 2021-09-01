fc6d5c6076a5ccb3233bceaeffcc1c8dc1e1ff5e
Fatih Degirmenci <fdegir@gmail.com>
2021-09-01 13:28:59 +0200

    Update action

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -l
+#!/bin/sh
 
 EIFFEL_MP=$1
 echo "Info  : Querying Eiffel REMReM Generate Service for list of Eiffel Events for Message Protocol $EIFFEL_MP"
@@ -8,3 +8,10 @@ echo "Info  : Command to run is"
 echo "        $REMREM_COMMAND"
 event_list=$($REMREM_COMMAND)
 echo "::set-output name=event_list::$event_list"
+
+echo "Info  : Environment variables"
+echo "-------------------------------------------------"
+env
+#echo HOME -e GITHUB_JOB -e GITHUB_REF -e GITHUB_SHA -e GITHUB_REPOSITORY -e GITHUB_REPOSITORY_OWNER -e GITHUB_RUN_ID -e GITHUB_RUN_NUMBER -e GITHUB_RETENTION_DAYS -e GITHUB_ACTOR -e GITHUB_WORKFLOW -e GITHUB_HEAD_REF -e GITHUB_BASE_REF -e GITHUB_EVENT_NAME -e GITHUB_SERVER_URL -e GITHUB_API_URL -e GITHUB_GRAPHQL_URL -e GITHUB_WORKSPACE -e GITHUB_ACTION -e GITHUB_EVENT_PATH -e GITHUB_ACTION_REPOSITORY -e GITHUB_ACTION_REF -e GITHUB_PATH -e GITHUB_ENV -e RUNNER_OS -e RUNNER_TOOL_CACHE -e RUNNER_TEMP -e RUNNER_WORKSPACE -e ACTIONS_RUNTIME_URL -e ACTIONS_RUNTIME_TOKEN -e ACTIONS_CACHE_URL
+echo "-------------------------------------------------"
+echo "Info  : Done!"
